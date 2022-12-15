41c5b2f0446ca42d977bff09fc04960a8cf11dab
Qrelos <egormacarov2@gmail.com>
2022-12-15 22:37:12 +0200

    Second commit

--- main.py
+++ main.py
@@ -8,13 +8,10 @@ bot = telebot.TeleBot(config.telegram_token)
 @bot.message_handler()
 def add(audio):
     bot.register_next_step_handler(audio, add)
-    urlvideo = audio.text
-    if urlvideo is None:
-        bot.send_message(audio.chat.id, 'Принимаются только ссылки на ютуб')
-    elif urlvideo.find('youtu') < 0:
+    if audio.text is None or audio.text.find('youtu') < 0:
         bot.send_message(audio.chat.id, 'Принимаются только ссылки на ютуб')
     else:
-        yt = YouTube(urlvideo)
+        yt = YouTube(audio.text)
         bot.send_message(audio.chat.id, yt.title)
         stream = yt.streams.get_by_itag(139)
         audioz = stream.download()
